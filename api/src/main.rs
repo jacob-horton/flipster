04b2ac4603a53e4c869b9e4bb8400f0adeb73b9d
Jacob Horton <hortonjacob1@gmail.com>
2023-05-10 21:28:56 +0100

    Project set up

---
+++ api/src/main.rs
@@ -0,0 +1,40 @@
+use std::{env, sync::Arc};
+
+use actix_cors::Cors;
+use actix_web::{http, web::Data, App, HttpServer};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
+#[derive(Clone, Debug)]
+pub struct AppState {
+    db_pool: Arc<Pool<Postgres>>,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    let db_url = env::var("DATABASE_URL").unwrap();
+    let db_pool = Arc::new(
+        PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&db_url)
+            .await
+            .unwrap(),
+    );
+
+    HttpServer::new(move || {
+        // TODO: properly configure cors
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_header(http::header::CONTENT_TYPE);
+
+        let state = AppState {
+            db_pool: db_pool.clone(),
+        };
+
+        App::new().app_data(Data::new(state)).wrap(cors)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
+}
---
+++ web/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1 className="text-center text-2xl p-10">Flipster</h1>;
+};
+
+export default App;
