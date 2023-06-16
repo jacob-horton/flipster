befa64476af7f90b9420d249cc0381e292cc1b1a
Duskt <30536860+Duskt@users.noreply.github.com>
2023-06-16 20:37:24 +0100

    Add icons
    icons added for:
     - Navbar (passed into NavbarTabs)
     - Profile
     - Login

--- web/src/components/Login.tsx
+++ web/src/components/Login.tsx
@@ -1,17 +1,18 @@
 import { useAuth } from "react-oidc-context";
+import { CgProfile } from "react-icons/cg";
 
 const Login = () => {
   const auth = useAuth();
 
   return (
     <button
-      className="flex space-x-4 items-center"
+      className="flex space-x-2 items-center"
       onClick={() =>
         auth.isAuthenticated ? auth.signoutSilent() : auth.signinRedirect()
       }
     >
       {/* TODO: profile icon */}
-      <div className="w-6 h-6 bg-gray-500" />
+      <CgProfile />
       <p>
         {auth.isAuthenticated
           ? `${auth.user?.profile.preferred_username} (Logout)`
--- web/src/components/NavbarTab.tsx
+++ web/src/components/NavbarTab.tsx
@@ -2,16 +2,20 @@ import Link from "next/link";
 
 interface NavbarTabProps {
   selected: boolean;
+  icon: React.ReactNode;
   name: string;
   path: string;
 }
 
-const NavbarTab: React.FC<NavbarTabProps> = ({ selected, name, path }) => {
+const NavbarTab: React.FC<NavbarTabProps> = ({
+  selected,
+  icon,
+  name,
+  path,
+}) => {
   return (
     <Link href={path} className="flex flex-row items-center space-x-2">
-      <div
-        className={`w-8 h-8 ${selected ? "bg-orange-400" : "bg-gray-400"}`}
-      />
+      <div className={selected ? "text-orange-400" : undefined}>{icon}</div>
       <p className={selected ? "text-orange-400" : undefined}>{name}</p>
     </Link>
   );
