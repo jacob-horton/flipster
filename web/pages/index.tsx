f34ddef3980e0854e63fcf4b1bd95b0b2f1e05f2
Duskt <30536860+Duskt@users.noreply.github.com>
2023-06-16 16:55:53 +0100

    Create navbar components
    - Contains Logo, NavbarTabs, Profile
    - Logo is a placeholder for the icon which will eventually redirect to index
    - NavbarTab can be selected and appears orange
    - Profile contains login button and achievement and profile icons (currently placeholders)

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,35 +1,8 @@
 import React from "react";
 import { useEffect } from "react";
 import { useAuth } from "react-oidc-context";
+import Profile from "../src/components/Profile"
 
 export default function Index() {
-  const auth = useAuth();
-
-  useEffect(() => {
-    if (auth.isAuthenticated) {
-      console.log("auth");
-    }
-  }, [auth.isAuthenticated]);
-
-  return (
-    <div className="p-2">
-      <h1 className="text-center text-2xl p-10">Flipster</h1>
-      {!auth.isAuthenticated && (
-        <button
-          className="bg-gray-300 rounded-lg px-4 py-1 border-opacity-50 border-gray-400 border"
-          onClick={() => auth.signinRedirect()}
-        >
-          Login
-        </button>
-      )}
-      {auth.isAuthenticated && (
-        <button
-          className="bg-gray-300 rounded-lg px-4 py-1 border-opacity-50 border-gray-400 border"
-          onClick={() => auth.signoutSilent()}
-        >
-          Logout ({auth.user?.profile.preferred_username})
-        </button>
-      )}
-    </div>
-  );
+    return <div />;
 }
---
+++ web/src/components/Logo.tsx
@@ -0,0 +1,7 @@
+const Logo = () => {
+    return <div>
+        <div className="w-12 h-12 bg-red-500"/>
+    </div>;
+}
+
+export default Logo
