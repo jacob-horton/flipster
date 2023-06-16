142e6aab2b39f4a5c07157d5d84f96cc00e64c01
Rhys Belcher <rb2262@bath.ac.uk>
2023-06-16 19:55:48 +0100

    Added icons

--- web/pages/files.tsx
+++ web/pages/files.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import PageSection from "../src/components/PageSection";
+import { FiFolder } from "react-icons/fi";
+import { FiFolderPlus } from "react-icons/fi";
 
 export default function Files() {
   return (
@@ -17,21 +19,35 @@ export default function Files() {
     >
       <div className="flex">
         <div className="flex-1 space-x-4 px-4">
-          <button className="p-4 bg-gray-800 text-white mb-4 rounded-lg">
+          <button className="p-4 text-gray-800 mb-4 rounded-lg">
             <div className="flex">
-              <span className="text-lg">Folder Icon</span>
+              <span className="text-lg">
+                <FiFolder
+                  size={80}
+                  strokeWidth={1}
+                  className="text-violet-600"
+                />
+              </span>
             </div>
             <div className="mt-auto">Physics</div>
           </button>
-          <button className="p-4 bg-gray-800 text-white rounded-lg mb-4">
+          <button className="p-4 text-gray-800 mb-4 rounded-lg">
             <div className="flex">
-              <span className="text-lg">Folder Icon</span>
+              <span className="text-lg">
+                <FiFolder
+                  size={80}
+                  strokeWidth={1}
+                  className="text-violet-600"
+                />
+              </span>
             </div>
             <div className="mt-auto">Poetry</div>
           </button>
-          <button className="p-4 bg-gray-800 text-white rounded-lg">
+          <button className="p-4 text-gray-800 mb-4 rounded-lg">
             <div className="flex">
-              <span className="text-lg">Add Folder </span>
+              <span className="text-lg">
+                <FiFolderPlus size={80} strokeWidth={1} strokeDasharray={2} />{" "}
+              </span>
             </div>
           </button>
         </div>
