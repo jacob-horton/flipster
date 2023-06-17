ed07effe33ff9cdd41ac909906e1a9743694b564
Jacob Horton <hortonjacob1@gmail.com>
2023-06-17 01:44:37 +0100

    MVP for flashcards

--- api/src/routes/user.rs
+++ api/src/routes/user.rs
@@ -1,20 +1,15 @@
-// #[post("/user/add")]
-// pub async fn add_user(
-//     data: Data<AppState>,
-//     payload: web::Json<UserInsert>,
-// ) -> Result<HttpResponse, Error> {
-//     let mut transaction = data
-//         .db_pool
-//         .begin()
-//         .await
-//         .expect("Failed to create transaction when adding flashcard");
-//
-//     // Create top level folder for user
-//     let folder_id = sqlx::query!(
-//         "INSERT INTO folder (name) VALUES ($1) RETURNING id",
-//         format!("user_{}_top_level", payload.user_id)
-//     )
-//     .fetch_one(&mut transaction)
-//     .await
-//     .expect("Failed to add top level folder for user");
-// }
+use actix_web::{get, web::Data, HttpRequest, HttpResponse, Responder};
+
+use crate::{utils, AppState};
+
+#[get("/user/top_level_folder")]
+pub async fn get_top_level_folder(data: Data<AppState>, req: HttpRequest) -> impl Responder {
+    let user_id = utils::get_user_id(&req).unwrap();
+
+    let user = sqlx::query!("SELECT flashcards FROM app_user WHERE id = $1", user_id)
+        .fetch_one(data.db_pool.as_ref())
+        .await
+        .unwrap();
+
+    HttpResponse::Ok().json(user.flashcards)
+}
