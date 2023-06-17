5f9548585e4cdda1af0b77ae040014ea964c7999
Rhys Belcher <rb2262@bath.ac.uk>
2023-06-17 14:40:48 +0100

    get subfolder names

--- web/src/apiRequest.ts
+++ web/src/apiRequest.ts
@@ -1,22 +1,35 @@
 // TODO: query params?
-interface RequestData {
+interface PostRequestData {
   path: string;
   id_token: string;
   payload?: string;
 }
 
-export async function getRequest(data: RequestData) {
-  return await fetch(`${process.env.NEXT_PUBLIC_API_URL}${data.path}`, {
+interface GetRequestData {
+  path: string;
+  id_token: string;
+  queryParams?: { parameter: string; val: string }[];
+}
+
+export async function getRequest(data: GetRequestData) {
+  const url = new URL(`${process.env.NEXT_PUBLIC_API_URL}${data.path}`);
+
+  if (data.queryParams !== undefined) {
+    for (const { parameter, val } of data.queryParams) {
+      url.searchParams.append(parameter, val);
+    }
+  }
+
+  return await fetch(url, {
     method: "GET",
     headers: {
       Authorization: "Bearer " + data.id_token,
       "Content-Type": "application/json",
     },
-    body: data.payload,
   });
 }
 
-export async function postRequest(data: RequestData) {
+export async function postRequest(data: PostRequestData) {
   return await fetch(`${process.env.NEXT_PUBLIC_API_URL}${data.path}`, {
     method: "POST",
     headers: {
