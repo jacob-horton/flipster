ea6c9e46b279be9ab09949dbbdc68dfd384fc224
Jacob Horton <hortonjacob1@gmail.com>
2023-06-17 20:16:02 +0100

    Remove request to `/test`, fix AuthProvider

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,37 +1,13 @@
 import React from "react";
 import { AuthProvider } from "react-oidc-context";
 import keycloak from "../src/keycloak";
-import { User } from "oidc-client-ts";
-import getPublicURL from "../src/getPublicURL";
 import "./globals.css";
 import Navbar from "../src/components/navbar/Navbar";
 
 const App = ({ Component, pageProps }) => {
   return (
     <React.StrictMode>
-      <AuthProvider
-        {...keycloak}
-        redirect_uri={getPublicURL("")}
-        onSigninCallback={async (a) => {
-          // TODO: remove, but keep function (documentation says it needs to be defined for auto refresh or smth)
-          if (a instanceof User) {
-            console.log(a.id_token);
-            console.log(`${process.env.NEXT_PUBLIC_API_URL}/test`);
-            const result = await fetch(
-              `${process.env.NEXT_PUBLIC_API_URL}/test`,
-              {
-                method: "GET",
-                headers: {
-                  Authorization: "Bearer " + a.id_token,
-                  "Content-Type": "application/json",
-                },
-              }
-            );
-            console.log(result.status);
-            console.log(result.body);
-          }
-        }}
-      >
+      <AuthProvider {...keycloak}>
         <div className="flex flex-col h-screen bg-gray-100">
           <Navbar />
           <Component {...pageProps} />
