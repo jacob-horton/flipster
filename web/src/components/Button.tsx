856809ec540ca81766eb3a4ac741a646274a83c5
Duskt <30536860+Duskt@users.noreply.github.com>
2023-07-16 21:32:09 +0100

    add Link functionality to native Button component

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,22 +1,47 @@
+import Link from "next/link";
 import React, { ReactNode } from "react";
 import { MouseEventHandler } from "react";
 
-interface ButtonProps {
-    children?: ReactNode;
+interface LinkButtonProps {
+    href: string;
+    query: {
+        [param: string]: string | string[] | boolean;
+    };
     onClick?: MouseEventHandler;
-    className?: string;
+    children?: ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick, className }) => {
+interface DefaultButtonProps {
+    onClick?: MouseEventHandler;
+    children?: ReactNode;
+}
+
+type ButtonProps = LinkButtonProps | DefaultButtonProps;
+
+function isLinkButton(obj: ButtonProps): obj is LinkButtonProps {
+    return "href" in obj;
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+    const { onClick, children } = props;
     return (
-        <div className={className}>
-            <button
-                className="bg-gray-300 px-5 rounded-lg light-border"
-                onClick={onClick}
-            >
-                {children}
-            </button>
-        </div>
+        <>
+            {!isLinkButton(props) ? (
+                <button
+                    className="light-border rounded-lg bg-gray-300 px-5"
+                    onClick={onClick}
+                >
+                    {children}
+                </button>
+            ) : (
+                <Link
+                    className="light-border rounded-lg bg-gray-300 px-5"
+                    href={{ pathname: props.href, query: props.query }}
+                >
+                    {children}
+                </Link>
+            )}
+        </>
     );
 };
 
