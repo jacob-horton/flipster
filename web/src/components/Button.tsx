8e4fb00f1ece4a9d4a33c2265cf16269771126d2
Duskt <30536860+Duskt@users.noreply.github.com>
2023-07-30 21:39:21 +0100

    revert link functionality

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -1,49 +1,23 @@
-import Link from "next/link";
 import React, { ReactNode } from "react";
 import { MouseEventHandler } from "react";
 
-interface LinkButtonProps {
-    href: string;
-    query: {
-        [param: string]: string | string[] | boolean;
-    };
-    onClick?: MouseEventHandler;
+interface ButtonProps {
     children?: ReactNode;
-}
-
-interface DefaultButtonProps {
     onClick?: MouseEventHandler;
-    children?: ReactNode;
-    type?: "button" | "submit" | "reset";
+    className?: string;
 }
 
-type ButtonProps = LinkButtonProps | DefaultButtonProps;
-
-function isLinkButton(obj: ButtonProps): obj is LinkButtonProps {
-    return "href" in obj;
-}
-
-const Button: React.FC<ButtonProps> = (props) => {
-    const { onClick, children } = props;
+// TODO: use styling
+const Button: React.FC<ButtonProps> = ({ children, onClick, className }) => {
     return (
-        <>
-            {!isLinkButton(props) ? (
-                <button
-                    className="light-border rounded-lg bg-gray-300 px-5"
-                    onClick={onClick}
-                    type={props.type}
-                >
-                    {children}
-                </button>
-            ) : (
-                <Link
-                    className="light-border rounded-lg bg-gray-300 px-5"
-                    href={{ pathname: props.href, query: props.query }}
-                >
-                    {children}
-                </Link>
-            )}
-        </>
+        <div className={className}>
+            <button
+                className="light-border rounded-lg bg-gray-300 px-5"
+                onClick={onClick}
+            >
+                {children}
+            </button>
+        </div>
     );
 };
 
