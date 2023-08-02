6d2060a2f6462d4dfd37301ec1d33cb1dd883597
Jacob Horton <hortonjacob1@gmail.com>
2023-08-02 20:35:37 +0100

    feat: useAxios to get axios with wrapper

---
+++ web/src/axiosWrapper.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosInstance } from "axios";
+import { useEffect, useState } from "react";
+import { useAuth } from "react-oidc-context";
+
+
+export function useAxios() {
+  const auth = useAuth();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [instance, setInstance] = useState<AxiosInstance>(
+    () => axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL })
+  );
+
+  useEffect(() => {
+    setInstance(
+      () => {
+        return axios.create({
+          baseURL: process.env.NEXT_PUBLIC_API_URL,
+          transformRequest: [(data, headers) => {
+            headers['Authorization'] = 'Bearer ' + auth.user?.id_token;
+            return data;
+          }]
+        })
+      }
+    )
+
+    if (!auth.isLoading) setIsLoading(false);
+  }, [auth.user?.id_token, auth.isLoading, setInstance])
+
+  return { axios: instance, isLoading };
+}
